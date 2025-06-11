e1c1c3ad746444ba573cf6f8171e18c1f5b9c023
“Muath <malasaa@umich.edu>
2025-06-11 18:07:55 -0400

    Update Hello.java in Moe branch

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hello World..." + i);
             System.out.println("Test1");
+            System.out.println("This is from Moe branch");
         }
     }
 }
